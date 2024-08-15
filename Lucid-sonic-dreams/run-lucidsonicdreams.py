8dbf95d8459f01872eb435be902e7bc9029e56b7
Inias <inias.somer@gmail.com>
2024-08-15 16:13:38 +0200

    Added notebooks and python scripts
    Working jupyter notebook of lucid sonic dreams + python script and added package versions

---
+++ Lucid-sonic-dreams/run-lucidsonicdreams.py
@@ -0,0 +1,18 @@
+from lucidsonicdreams import LucidSonicDream
+
+L = LucidSonicDream(song = '/content/yoursong.mp4', #mp3 here
+                    style = '/content/drive/MyDrive/StyleGAN2/wikiart.pkl') #pkl path here
+
+L.hallucinate(file_name = '/content/outputvideo.mp4', #output path here
+              start = 0,
+              duration=30,  
+              speed_fpm = 20,
+              pulse_percussive = True,
+              pulse_harmonic= False,
+              pulse_react = 1,
+              motion_harmonic = True,
+              motion_percussive = False,
+              motion_react = 0.5,
+              fps = 24, 
+              ) 
+     
