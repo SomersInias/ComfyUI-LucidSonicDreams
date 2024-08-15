35ac6891ba4a291a61ffb4b6815adb330a10a70b
Inias <inias.somer@gmail.com>
2024-08-15 16:47:36 +0200

    small change + added folders and readme

--- Lucid-sonic-dreams/run-lucidsonicdreams.py
+++ Lucid-sonic-dreams/run-lucidsonicdreams.py
@@ -1,6 +1,6 @@
 from lucidsonicdreams import LucidSonicDream
 
-L = LucidSonicDream(song = '/content/yoursong.mp4', #mp3 here
+L = LucidSonicDream(song = '/content/yoursong.mp4', #mp3,mp4,wav here
                     style = '/content/drive/MyDrive/StyleGAN2/wikiart.pkl') #pkl path here
 
 L.hallucinate(file_name = '/content/outputvideo.mp4', #output path here
